bd5b384f00b906fcde6c4c20f112104e59fabd4e
Jackie Nobbe <jac.nobbe@gmail.com>
2017-04-19 00:53:14 -0500

    working add Band, add show still needs work

--- project/m4/newBand.php
+++ project/m4/newBand.php
@@ -3,6 +3,137 @@
     // $_POST['ref'] = $here;
     // header("Location: login.php");
 else: ?> -->
+<?php
+    function print_form() {
+    echo <<<END
+        <h2>Gotta New Band? </h2>
+        <form id="new" action="$_SERVER[PHP_SELF]" method="post">
+          <div class="form-group"><label>Name <span class="required">*</span></label><input type="text" name="bandName" required /></div>
+          <div class="form-group"><label>Hometown <span class="required">*</span></label><input type="text" name="homeTown" required /></div>
+          <div class="form-group"><label>Website/Facebook <span class="required">*</span></label><input type="url" name="website" required /></div>
+          <div class="form-group"><label>Description <span class="required">*</span></label><input type="text" name="description" required /></div>
+          <div class="form-group"><label>Contact Person <span class="required">*</span></label><input type="text" name="contact" required /></div>
+          <div class="form-group"><label>Email </label><input type="email" name="contact" /></div>
+          <div class="form-group"><label># of Members</label><input type="text" name="numOfMembers" /></div>
+
+          <div class="form-group">
+            <label>Professionalism <span class="required">*</span></label><br/>
+              pain in the butt
+              <input type="range" min="1" max="5" value="3" step="1" name="professionalism" required />
+              business in the front
+          </div>
+          <div class="form-group">
+            <label>Music Quality <span class="required">*</span></label><br/>
+              painful 4 all parties
+              <input type="range" min="1" max="5" value="3" step="1" name="quality" required />
+              WOWMOM
+          </div>
+          <div class="form-group">
+            <label>Energy <span class="required">*</span></label><br/>
+              we checked 4 a pulse but...
+              <input type="range" min="1" max="5" value="3" step="1" name="energy" required />
+              just stuck a fork in an outlet
+          </div>
+
+          <div class="form-group">
+            <label>Genre</label><br/>
+              <select name="genre1">
+                <option value=""></option>
+                <option value="acoustic">acoustic</option>
+                <option value="alternative">alternative</option>
+                <option value="ambient">ambient</option>
+                <option value="bluegrass">bluegrass</option>
+                <option value="blues">blues</option>
+                <option value="chill">chill</option>
+                <option value="contemporary">contemporary</option>
+                <option value="country">country</option>
+                <option value="dance">dance</option>
+                <option value="downtempo">downtempo</option>
+                <option value="dub">dub</option>
+                <option value="electronic">electronic</option>
+                <option value="emo">emo</option>
+                <option value="folk">folk</option>
+                <option value="freeform">freeform</option>
+                <option value="funk">funk</option>
+                <option value="fusion">fusion</option>
+                <option value="grunge">grunge</option>
+                <option value="hard rock">hard rock</option>
+                <option value="heavy metal">heavy metal</option>
+                <option value="hip-hop">hip-hop</option>
+                <option value="house">house</option>
+                <option value="IDM">IDM</option>
+                <option value="indie rock">indie rock</option>
+                <option value="jam band">jam band</option>
+                <option value="jazz">jazz</option>
+                <option value="krautrock">krautrock</option>
+                <option value="lo-fi">lo-fi</option>
+                <option value="lounge">lounge</option>
+                <option value="metal">metal</option>
+                <option value="new age">new age</option>
+                <option value="pop">pop</option>
+                <option value="psychadelic">psychadelic</option>
+                <option value="punk">punk</option>
+                <option value="rap">rap</option>
+                <option value="reggae">reggae</option>
+                <option value="rock and roll">rock and roll</option>
+                <option value="singer songwriter">singer songwriter</option>
+                <option value="soft rock">soft rock</option>
+                <option value="soul">soul</option>
+                <option value="surf">surf</option>
+                <option value="techno">techno</option>
+              </select><br/>
+            </div>
+          <input type="hidden" name="dateAdded"  value="<?php Date() ?>" />
+          <input type="hidden" name="go" value="process">
+          <input type="submit" name="submit" value="Let's Go"/>
+        </form >
+        </div>
+END;
+    }
+
+    function process_form() {
+        $bandName = $_POST['bandName'];
+        $homeTown = $_POST['homeTown'];
+        $website = $_POST['website'];
+        $description = $_POST['description'];
+        $contactPerson = $_POST['contact'];
+        $email = $_POST['email'];
+        $professionalism = $_POST['professionalism'];
+        $quality = $_POST['quality'];
+        $energy = $_POST['energy'];
+        $genre1 = $_POST['genre1'];
+
+        try {
+            $conn = new PDO("mysql:host=mysql.truman.edu;dbname=jen1141CS315", "jen1141", "aeveuthu");
+
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // prepare sql and bind parameters
+            $stmt = $conn->prepare("INSERT INTO Band (bandName, homeTown, website, description, contact, email, professionalism, quality, energy)
+            VALUES (:bandName, :homeTown, :website, :description, :contact, :email, :professionalism, :quality, :energy)");
+            $stmt->bindParam(':bandName', $bandName);
+            $stmt->bindParam(':homeTown', $homeTown);
+            $stmt->bindParam(':website', $website);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':contact', $contactPerson);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':professionalism', $professionalism);
+            $stmt->bindParam(':quality', $quality);
+            $stmt->bindParam(':energy', $energy);
+
+            $stmt->execute();
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        // end the DB connection.
+        $conn = null;
+
+        echo "<p>Success!!!</p>";
+    }
+?>
 
 <!DOCTYPE html>
   <head>
@@ -15,178 +146,15 @@ else: ?> -->
     <div id="wrapper"> -->
       <?php include('includes/header.php'); ?>
        <div id="content">
-            <h2>Gotta New Band? </h2>
-            <form id="new" action="viewBand.php" method="post" onsubmit="return checkform(this)">
-              <div class="form-group"><label>Name <span class="required">*</span></label><input type="text" name="bandName" required /></div>
-              <div class="form-group"><label>Hometown <span class="required">*</span></label><input type="text" name="homeTown" required /></div>
-              <div class="form-group"><label>Website/Facebook <span class="required">*</span></label><input type="text" name="website" required /></div>
-              <div class="form-group"><label>Description <span class="required">*</span></label><input type="text" name="description" required /></div>
-              <div class="form-group"><label>Contact Person <span class="required">*</span></label><input type="text" name="contact" required /></div>
-              <div class="form-group"><label># of Members</label><input type="text" name="numOfMembers" /></div>
-
-              <div class="form-group">
-                <label>Professionalism <span class="required">*</span></label><br/>
-                  pain in the butt
-                  <input type="range" min="1" max="5" value="3" step="1" name="professionalism" required />
-                  business in the front
-              </div>
-              <div class="form-group">
-                <label>Music Quality <span class="required">*</span></label><br/>
-                  painful 4 all parties
-                  <input type="range" min="1" max="5" value="3" step="1" name="quality" required />
-                  WOWMOM
-              </div>
-              <div class="form-group">
-                <label>Energy <span class="required">*</span></label><br/>
-                  we checked 4 a pulse but...
-                  <input type="range" min="1" max="5" value="3" step="1" name="energy" required />
-                  just stuck a fork in an outlet
-              </div>
-
-              <div class="form-group">
-                <label>Genre (pick 3)</label><br/>
-                  <select name="genre1">
-                    <option value=""></option>
-                    <option value="acoustic">acoustic</option>
-                    <option value="alternative">alternative</option>
-                    <option value="ambient">ambient</option>
-                    <option value="bluegrass">bluegrass</option>
-                    <option value="blues">blues</option>
-                    <option value="chill">chill</option>
-                    <option value="contemporary">contemporary</option>
-                    <option value="country">country</option>
-                    <option value="dance">dance</option>
-                    <option value="downtempo">downtempo</option>
-                    <option value="dub">dub</option>
-                    <option value="electronic">electronic</option>
-                    <option value="emo">emo</option>
-                    <option value="folk">folk</option>
-                    <option value="freeform">freeform</option>
-                    <option value="funk">funk</option>
-                    <option value="fusion">fusion</option>
-                    <option value="grunge">grunge</option>
-                    <option value="hard rock">hard rock</option>
-                    <option value="heavy metal">heavy metal</option>
-                    <option value="hip-hop">hip-hop</option>
-                    <option value="house">house</option>
-                    <option value="IDM">IDM</option>
-                    <option value="indie rock">indie rock</option>
-                    <option value="jam band">jam band</option>
-                    <option value="jazz">jazz</option>
-                    <option value="krautrock">krautrock</option>
-                    <option value="lo-fi">lo-fi</option>
-                    <option value="lounge">lounge</option>
-                    <option value="metal">metal</option>
-                    <option value="new age">new age</option>
-                    <option value="pop">pop</option>
-                    <option value="psychadelic">psychadelic</option>
-                    <option value="punk">punk</option>
-                    <option value="rap">rap</option>
-                    <option value="reggae">reggae</option>
-                    <option value="rock and roll">rock and roll</option>
-                    <option value="singer songwriter">singer songwriter</option>
-                    <option value="soft rock">soft rock</option>
-                    <option value="soul">soul</option>
-                    <option value="surf">surf</option>
-                    <option value="techno">techno</option>
-                  </select><br/>
-                  <select name="genre2">
-                    <option value=""></option>
-                    <option value="acoustic">acoustic</option>
-                    <option value="alternative">alternative</option>
-                    <option value="ambient">ambient</option>
-                    <option value="bluegrass">bluegrass</option>
-                    <option value="blues">blues</option>
-                    <option value="chill">chill</option>
-                    <option value="contemporary">contemporary</option>
-                    <option value="country">country</option>
-                    <option value="dance">dance</option>
-                    <option value="downtempo">downtempo</option>
-                    <option value="dub">dub</option>
-                    <option value="electronic">electronic</option>
-                    <option value="emo">emo</option>
-                    <option value="folk">folk</option>
-                    <option value="freeform">freeform</option>
-                    <option value="funk">funk</option>
-                    <option value="fusion">fusion</option>
-                    <option value="grunge">grunge</option>
-                    <option value="hard rock">hard rock</option>
-                    <option value="heavy metal">heavy metal</option>
-                    <option value="hip-hop">hip-hop</option>
-                    <option value="house">house</option>
-                    <option value="IDM">IDM</option>
-                    <option value="indie rock">indie rock</option>
-                    <option value="jam band">jam band</option>
-                    <option value="jazz">jazz</option>
-                    <option value="krautrock">krautrock</option>
-                    <option value="lo-fi">lo-fi</option>
-                    <option value="lounge">lounge</option>
-                    <option value="metal">metal</option>
-                    <option value="new age">new age</option>
-                    <option value="pop">pop</option>
-                    <option value="psychadelic">psychadelic</option>
-                    <option value="punk">punk</option>
-                    <option value="rap">rap</option>
-                    <option value="reggae">reggae</option>
-                    <option value="rock and roll">rock and roll</option>
-                    <option value="singer songwriter">singer songwriter</option>
-                    <option value="soft rock">soft rock</option>
-                    <option value="soul">soul</option>
-                    <option value="surf">surf</option>
-                    <option value="techno">techno</option>
-                  </select><br/>
-                  <select name="genre3">
-                    <option value=""></option>
-                    <option value="acoustic">acoustic</option>
-                    <option value="alternative">alternative</option>
-                    <option value="ambient">ambient</option>
-                    <option value="bluegrass">bluegrass</option>
-                    <option value="blues">blues</option>
-                    <option value="chill">chill</option>
-                    <option value="contemporary">contemporary</option>
-                    <option value="country">country</option>
-                    <option value="dance">dance</option>
-                    <option value="downtempo">downtempo</option>
-                    <option value="dub">dub</option>
-                    <option value="electronic">electronic</option>
-                    <option value="emo">emo</option>
-                    <option value="folk">folk</option>
-                    <option value="freeform">freeform</option>
-                    <option value="funk">funk</option>
-                    <option value="fusion">fusion</option>
-                    <option value="grunge">grunge</option>
-                    <option value="hard rock">hard rock</option>
-                    <option value="heavy metal">heavy metal</option>
-                    <option value="hip-hop">hip-hop</option>
-                    <option value="house">house</option>
-                    <option value="IDM">IDM</option>
-                    <option value="indie rock">indie rock</option>
-                    <option value="jam band">jam band</option>
-                    <option value="jazz">jazz</option>
-                    <option value="krautrock">krautrock</option>
-                    <option value="lo-fi">lo-fi</option>
-                    <option value="lounge">lounge</option>
-                    <option value="metal">metal</option>
-                    <option value="new age">new age</option>
-                    <option value="pop">pop</option>
-                    <option value="psychadelic">psychadelic</option>
-                    <option value="punk">punk</option>
-                    <option value="rap">rap</option>
-                    <option value="reggae">reggae</option>
-                    <option value="rock and roll">rock and roll</option>
-                    <option value="singer songwriter">singer songwriter</option>
-                    <option value="soft rock">soft rock</option>
-                    <option value="soul">soul</option>
-                    <option value="surf">surf</option>
-                    <option value="techno">techno</option>
-                  </select><br/>
-                </div>
-              <!-- <input type="hidden" name="addedBy" value=<?php echo $_POST['currentUser']; ?> /> -->
-              <input type="hidden" name="dateAdded"  value="<?php Date() ?>" />
-              <input type="submit" name="submit" value="Let's Go"/>
-            </form >
-        </div>
-    </div>
+           <?php
+                if (isset($_POST['go']) && ('process' == $_POST['go'])) {
+                    process_form();
+                }
+                else {
+                    print_form();
+                }
+            ?>
+       </div>
   </body>
 </html>
 <!-- <?php endif; ?> -->
--- project/m4/newShow.php
+++ project/m4/newShow.php
@@ -1,3 +1,99 @@
+<?php
+    function print_form() {
+    echo <<<END
+        <h2>Gotta New Show? </h2>
+        <form id="new" action="$_SERVER[PHP_SELF]" method="post" >
+          <div class="form-group"><label>Name <span class="required">*</span></label><input type="text" name="showName" id="showName" class="" required /></div>
+          <div class="form-group"><label>Theme <span class="required">*</span></label><input type="text" name="theme" id="theme" class="" required /></div>
+          <div class="form-group"><label>FB Event Link <span class="required">*</span></label><input type="url" name="fbLink" id="fbLink" class="" required /></div>
+          <div class="form-group"><label>Date <span class="required">*</span></label><input type="text" name="showDate" id="showDate" class="" required /></div>
+          <div class="form-group"><label>Location <span class="required">*</span></label><input type="text" name="location" id="location" class="" required /></div>
+          <div class="form-group"><label>Door Price <span class="required">*</span></label><input type="text" name="doorPrice" id="doorPrice" class="" required /></div>
+          <div class="form-group"><label>Crowd Size</label><input type="text" name="crowdSize" id="crowdSize" class="" /></div>
+          <div class="form-group"><label>$ Made</label><input type="text" name="profit" id="profit" class="" /></div>
+          <div class="form-group"><label>Potluck? </label><input type="checkbox" name="potluck" value="yes" id="potluck" class="" /></div>
+          <div class="form-group"><label>Planned By <span class="required">*</span></label><input type="text" name="plannedBy" id="plannedBy" class="" required /></div>
+
+          <div class="form-group">
+            <label>Cohesiveness <span class="required">*</span></label><br/>
+              the feel didn&#39;t flow
+              <input type="range" min="1" max="5" value="3" step="1" name="cohesiveness" id="cohesiveness" class="" required />
+              it all just fit<br/>
+          </div>
+          <div class="form-group">
+            <label>Well Advirtised? <span class="required">*</span></label><br/>
+              oops
+              <input type="range" min="1" max="5" value="3" step="1" name="advertisement" id="advertisement" class="" required />
+              heckas adverts<br/>
+          </div>
+          <div class="form-group">
+            <label>What went well? <span class="required">*</span></label><br/>
+              <textarea name="wentWell" id="wentWell" class="" required ></textarea><br/>
+          </div>
+          <div class="form-group">
+            <label>What could have been better? <span class="required">*</span></label><br/>
+              <textarea name="wentBadly" id="wentBadly" class="" required ></textarea><br/>
+          </div>
+          <input type="hidden" name="go" value="process">
+          <input type="submit" name="submit" value="Let's Go"/>
+        </form>
+       </div>
+END;
+    }
+
+    function process_form() {
+        $showName = $_POST['showName'];
+        $showDate = $_POST['showDate'];
+        $fbLink = $_POST['fbLink'];
+        $location = $_POST['location'];
+        $crowdSize = $_POST['crowdSize'];
+        $theme = $_POST['theme'];
+        $moneyMade = $_POST['profit'];
+        $doorPrice = $_POST['doorPrice'];
+        $potluck = $_POST['potluck'];
+        $plannedBy = $_POST['plannedBy'];
+        $cohesiveness = $_POST['cohesiveness'];
+        $advirtisement = $_POST['advirtisement'];
+        $wentWell = $_POST['wentWell'];
+        $wentBad = $_POST['wentBad'];
+
+        try {
+            $conn = new PDO("mysql:host=mysql.truman.edu;dbname=jen1141CS315", "jen1141", "aeveuthu");
+
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // prepare sql and bind parameters
+            $stmt = $conn->prepare("INSERT INTO Show (showName, showDate, fbLink, location, crowdSize, theme, moneyMade, doorPrice, potluck, plannedBy, cohesiveness, advertisement, wentWell, wentBad)
+            VALUES (:showName, :showDate, :fbLink, :location, :crowdSize, :theme, :moneyMade, :doorPrice, :potluck, :plannedBy, :cohesiveness, :advertisement, :wentWell, :wentBad)");
+            $stmt->bindParam(':showName', $showName);
+            $stmt->bindParam(':showDate', $showDate);
+            $stmt->bindParam(':fbLink', $fbLink);
+            $stmt->bindParam(':location', $location);
+            $stmt->bindParam(':crowdSize', $crowdSize);
+            $stmt->bindParam(':theme', $theme);
+            $stmt->bindParam(':moneyMade', $moneyMade);
+            $stmt->bindParam(':doorPrice', $doorPrice);
+            $stmt->bindParam(':potluck', $potluck);
+            $stmt->bindParam(':plannedBy', $plannedBy);
+            $stmt->bindParam(':cohesiveness', $cohesiveness);
+            $stmt->bindParam(':advertisement', $advertisement);
+            $stmt->bindParam(':wentWell', $wentWell);
+            $stmt->bindParam(':wentBad', $wentBad);
+
+            $stmt->execute();
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        // end the DB connection.
+        $conn = null;
+
+        echo "<p>Success!!!</p>";
+    }
+?>
+
 <!DOCTYPE html>
   <head>
     <title>aqua-archive // add a show</title>
@@ -8,44 +104,14 @@
 
       <?php include('includes/header.php'); ?>
       <div id="content">
-          <h2>Gotta New Show? </h2>
-          <form id="new" action="viewShow.php" method="post" >
-            <div class="form-group"><label>Name <span class="required">*</span></label><input type="text" name="showName" id="showName" class="" required /></div>
-            <div class="form-group"><label>Date <span class="required">*</span></label><input type="text" name="showDate" id="showDate" class="" required /></div>
-            <div class="form-group"><label>Location <span class="required">*</span></label><input type="text" name="location" id="location" class="" required /></div>
-            <div class="form-group"><label>Door Price <span class="required">*</span></label><input type="text" name="doorPrice" id="doorPrice" class="" required /></div>
-            <div class="form-group"><label>Crowd Size</label><input type="text" name="crowdSize" id="crowdSize" class="" /></div>
-            <div class="form-group"><label>$ Made</label><input type="text" name="profit" id="profit" class="" /></div>
-            <div class="form-group"><label>Facebook Event <span class="required">*</span></label><input type="text" name="fbevent" id="fbevent" class="" required /></div>
-            <div class="form-group"><label>Potluck? </label><input type="checkbox" name="potluck" value="yes" id="potluck" class="" /></div>
-            <div class="form-group"><label>Planned By <span class="required">*</span></label><input type="text" name="plannedBy" id="plannedBy" class="" required /></div>
-
-            <div class="form-group">
-              <label>Cohesiveness <span class="required">*</span></label><br/>
-                the feel didn&#39;t flow
-                <input type="range" min="1" max="5" value="3" step="1" name="cohesivness" id="cohesivness" class="" required />
-                it all just fit<br/>
-            </div>
-            <div class="form-group">
-              <label>Well Advirtised? <span class="required">*</span></label><br/>
-                oops
-                <input type="range" min="1" max="5" value="3" step="1" name="advertisment" id="advertisment" class="" required />
-                heckas adverts<br/>
-            </div>
-            <div class="form-group">
-              <label>What went well? <span class="required">*</span></label><br/>
-                <textarea name="wentWell" id="wentWell" class="" required ></textarea><br/>
-            </div>
-            <div class="form-group">
-              <label>What could have been better? <span class="required">*</span></label><br/>
-                <textarea name="wentBadly" id="wentBadly" class="" required ></textarea><br/>
-            </div>
-            <!-- <input type="hidden" name="addedBy" value="<?php echo $_POST['currentUser']; ?>" /> -->
-            <input type="submit" name="submit" value="Let's Go"/>
-          </form>
+          <?php
+               if (isset($_POST['go']) && ('process' == $_POST['go'])) {
+                   process_form();
+               }
+               else {
+                   print_form();
+               }
+           ?>
       </div>
-    </div>
-
   </body>
-
 </html>
